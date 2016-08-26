462e82d4cccb0b7c61ca5f64d32b0e90edcc2c34
Guler Hayg <hayg.guler@gmail.com>
2016-08-26 18:34:44 +0200

    dernière motif avec boucle qui permet de séparer les différentes périodes. La suite est de mettre en place le fit expo.

--- Ana_Ref_ZeroRef.m
+++ Ana_Ref_ZeroRef.m
@@ -1,5 +1,6 @@
 function[Corr_data,timeFull,chanFull,chanRef,delta_temp,raw_max_jump] =  Ana_Ref_ZeroRef(refData,chanData,Time,min1,max1,butter_cut,min2)
 close all
+Nsensor=9 
 
 
 %% try to combine the analysis into a single file
@@ -15,51 +16,61 @@ Granular_sensor_positions=[13,8,14,18,12,3,15,23,11];
 
 size(chanFull)
 
-subplot = @(m,n,p) subtightplot (m, n, p, [0.04 0.02 0.01], [0.04 0.02 0.01], [0.04 0.02 0.01]);
-I_Axis_limits=[-0.2,0.3];
-%I_Axis_limits=[0.2,0.7];
-I_Axis_limits=[-2,9];
-I_Axis_limits=[20,30];
+% subplot = @(m,n,p) subtightplot (m, n, p, [0.04 0.02 0.01], [0.04 0.02 0.01], [0.04 0.02 0.01]);
+% I_Axis_limits=[-0.2,0.3];
+% I_Axis_limits=[0.2,0.7];
+% I_Axis_limits=[-2,9];
+% I_Axis_limits=[20,30];
+I_Axis_limits=[-1,1];
 
  Nbins_sec=6000;
 % Nbins_sec=12000;
 % Nbins_sec=2000;
- time_shift=558.3;
- %time_shift=1.73;
- time_shift=5280;
- time_shift=0;
+% time_shift=558.3;
+% time_shift=1.73;
+% time_shift=5280;
+
+time_shift=0;
  
  
-Nsensor=9 
-plo_id=[9,5,1,3,7]
+% plo_id=[9,5,1,3,7]
 plo_id=[1:9]
 % for i=1:9
+
+max_jump=zeros(Nsensor,floor((max1-min2)/Nbins_sec)+1);
+raw_max_jump=zeros(Nsensor,floor((max1-min2)/Nbins_sec)+1);
+
+min_temp=zeros(Nsensor);
+max_temp=zeros(Nsensor);
+delta_temp=zeros(Nsensor);
+corr_temp=zeros(Nsensor);
+
 for i=1:Nsensor
     j=plo_id(i);
     %% --- FFT -----------------
-    signal_fft=fft(chanFull(1,:,j));
+    % ----- NOT USED ----    signal_fft=fft(chanFull(1,:,j));
     %% Low pass filter
     [b,a]=butter(8,[butter_cut]/(1000),'low');
     lowPassedData = filter(b,a,chanFull(1,:,j));
+    
     Corr_data(i,:,:)=lowPassedData(:,:);
     figure(115)
     hold on
-
+%% --------------- MIN and MAX temp ---- NOT USED YET ------------
     min_temp(i)=min(15*lowPassedData(1,min1:max1));
     max_temp(i)=max(15*lowPassedData(1,min1:max1));
     delta_temp(i)= max_temp(i)-min_temp(i);
+%% ---------------------------------------------------------------
 
-    corr_temp(i)=mean(lowPassedData(1,200:400) );
+    corr_temp(i)=mean(lowPassedData(1,200:400) ); %% ---  ?? pourquoi ? 
     
- %   plot(timeFull(:,min1:max1)-time_shift,15*( lowPassedData(1,min1:max1) - mean(lowPassedData(1,200:400))),'linewidth',2,'color',col(5*i-1,:));
-    plot(min1:max1,                       15*( lowPassedData(1,min1:max1) - mean(lowPassedData(1,min1:min1+100))),'linewidth',2,'color',col(5*i-1,:));
- 
-    
+   plot(timeFull(:,min1:max1) -  time_shift,15*( lowPassedData(1,min1:max1) - mean(lowPassedData(1,min1:min1+100))),'linewidth',2,'color',col(5*i-1,:));
+%     plot(min1:max1,                       15*( lowPassedData(1,min1:max1) - mean(lowPassedData(1,min1:min1+100))),'linewidth',2,'color',col(5*i-1,:));
 %% Calculs dans les intervalles de periode du faisceau (ex : 1s ON, 5s OFF = 6s la periode)    
-%% pour les fits il faudra plutot sauver pour chaque intervalle la points dans un tableau qui sera fitté 
+%% pour les fits il faudra plutot sauver pour chaque intervalle la points dans un tableau qui sera fitt? 
    for ibin=1:floor((max1-min2)/Nbins_sec)+1
-      max_jump(i,ibin)=15*max(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec))-corr_temp(i);
-      raw_max_jump(i,ibin)=15*max(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec))-15*min(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec));
+      max_jump(i,ibin)     = 15*max(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec))-corr_temp(i);
+      raw_max_jump(i,ibin) = 15*max(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec))-15*min(lowPassedData(1,min2+(ibin-1)*Nbins_sec:min2+(ibin)*Nbins_sec));
    end
     
  %   ylim(I_Axis_limits);
@@ -68,13 +79,13 @@ for i=1:Nsensor
     xlhand = get(gca,'xlabel');
     set(xlhand,'string','Time [sec]','fontsize',20);  
     ylhand = get(gca,'ylabel');
-    set(ylhand,'string','Temp [°C]','fontsize',20);  
+    set(ylhand,'string','Temp [?C]','fontsize',20);  
     legendInfo{i} = ['Sensor No ' num2str(j)];
 end
 for i=1:floor((max1-min2)/Nbins_sec)+1
     bin_droite=min2+(i-1)*Nbins_sec  ;
-%    plot([timeFull(:,bin_droite)- time_shift,timeFull(:,bin_droite)- time_shift],I_Axis_limits,'-')    
-    plot([bin_droite,bin_droite]- time_shift,I_Axis_limits,'-')    
+    plot([timeFull(:,bin_droite)- time_shift,timeFull(:,bin_droite)- time_shift], I_Axis_limits,'-')    
+%   plot([bin_droite,bin_droite]- time_shift                                    , I_Axis_limits,'-')    
 end
 
 %%% Ref data -- 
